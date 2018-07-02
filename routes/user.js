be32e195ced39a4d16d0556d37f58036602c7989
matengfei <1341555203@qq.com>
2018-07-02 17:52:24 +0800

    add

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const userService = require('../service/userService')
 
-router.get('/login', function (req, res) {
+router.post('/login', function (req, res) {
     const user_service  = new userService()
-    let username = req.param('username')
-    let password = req.param('password')
+    let username = req.body.username//param('username')
+    let password = req.body.password// param('password')
     if(username && password){
         user_service.login(username, password)
             .then(data => {
